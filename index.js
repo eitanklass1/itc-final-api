9a00f1289624946542ac81778ea90b75013a972b
eitanklass1 <59514595+eitanklass1@users.noreply.github.com>
2024-02-14 22:25:26 -0600

    cors error fix

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const score = require("./routes/scoreRoute");
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+// app.use(cors());
 
 //Routes
 
